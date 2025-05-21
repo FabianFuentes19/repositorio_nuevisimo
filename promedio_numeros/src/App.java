b73556f4cec86a71e3892da1b4ff7667c0e7bacd
FabianFuentes19 <20243ds032@utesz.edu.mx>
2025-05-21 08:17:58 -0600

    como segundo movimiento se pidio al usuario ingresar el segundo numero

--- promedio_numeros/src/App.java
+++ promedio_numeros/src/App.java
@@ -4,5 +4,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         String numero1 = JOptionPane.showInputDialog("ingresa el primer numero: ");
         System.out.println(Integer.parseInt(numero1));
+
+        String numero2 = JOptionPane.showInputDialog("ingresa el segundo numero: ");
+        System.out.println(Integer.parseInt(numero2));
+
+
     }
 }

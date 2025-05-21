2f80130655e7418d00a4dd12e1770b7d864b7248
FabianFuentes19 <20243ds032@utesz.edu.mx>
2025-05-21 08:59:58 -0600

    ultimo movimiento, se hizo la operacion para sacar el promedio, y se le muestra al usuario en un cuadro emergente

--- promedio_numeros/src/App.java
+++ promedio_numeros/src/App.java
@@ -1,16 +1,21 @@
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String numero1 = JOptionPane.showInputDialog("ingresa el primer numero: ");
-        System.out.println(Integer.parseInt(numero1));
+        
+        Scanner scanner = new Scanner(System.in);
+        String numero = JOptionPane.showInputDialog("Ingrese el primer número: ");
+        float num1 = Float.parseFloat(numero);
+        String numeroo = JOptionPane.showInputDialog("Ingrese el segundo número: ");
+        float num2 = Float.parseFloat(numeroo);
+        String numerooo = JOptionPane.showInputDialog("Ingrese el tercer número: ");
+        float num3 = Float.parseFloat(numerooo);
 
-        String numero2 = JOptionPane.showInputDialog("ingresa el segundo numero: ");
-        System.out.println(Integer.parseInt(numero2));
+        float promedio = (num1 + num2 + num3) / 3;
 
-        String numero3 = JOptionPane.showInputDialog("ingresa el tercer numero: ");
-        System.out.println(Integer.parseInt(numero3));
-
-
+        System.out.println("El promedio es; " + promedio);
+        JOptionPane.showMessageDialog(null, "El promedio es: " + promedio);
+        
     }
 }
